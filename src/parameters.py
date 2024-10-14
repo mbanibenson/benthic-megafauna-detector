95b5841d71c195168491c93b1b0da87fd9825eb0
mbani <onyangombani@gmail.com>
2024-10-14 20:00:44 +0200

    Define a new custom data source

--- src/parameters.py
+++ src/parameters.py
@@ -1,8 +1,11 @@
 
 
 #DIRECTORY_WITH_LABELED_TRAINING_EXAMPLES =  '/mnt/d/projects/benthic-megafauna-detector/datasets/savanna_taxa'
+#DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/benthic-megafauna-detector/datasets/savanna_taxa/images/test'
+
 DIRECTORY_WITH_LABELED_TRAINING_EXAMPLES =  '/mnt/d/projects/cjgea/datasets/annotated/task20/ultralytics_yolo'
-DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/benthic-megafauna-detector/datasets/savanna_taxa/images/test'
+DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/cjgea/datasets/Watamu Marine Park/WMP_21_06_2024_underwater_images'
+
 NUMBER_OF_EPOCHS = 10
 IMAGE_SIZE = 640
 
