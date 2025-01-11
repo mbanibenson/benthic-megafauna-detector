cfa5d1df4d1d23f35001dd3f9cd2b97b409aefe3
mbani <onyangombani@gmail.com>
2025-01-11 15:13:43 +0300

    Clean up uncommitted files

--- src/parameters.py
+++ src/parameters.py
@@ -3,8 +3,8 @@
 #DIRECTORY_WITH_LABELED_TRAINING_EXAMPLES =  '/mnt/d/projects/benthic-megafauna-detector/datasets/savanna_taxa'
 #DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/benthic-megafauna-detector/datasets/savanna_taxa/images/test'
 
-DIRECTORY_WITH_LABELED_TRAINING_EXAMPLES =  '/mnt/d/projects/cjgea/datasets/annotated/task20/ultralytics_yolo'
-DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/cjgea/datasets/to_detect'
+DIRECTORY_WITH_LABELED_TRAINING_EXAMPLES =  '/mnt/d/projects/cjgea/datasets/annotated/task13/ultralytics_yolo'
+DIRECTORY_WITH_IMAGES_TO_BE_ANNOTATED = '/mnt/d/projects/cjgea/datasets/Watamu Marine Park/WMP_21_06_2024_underwater_videos'
 
 NUMBER_OF_EPOCHS = 10
 IMAGE_SIZE = 640
